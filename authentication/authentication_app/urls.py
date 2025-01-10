69cf536e20794e4a8752fd080f4179e7d5e597e8
Dhrumeen Patel <dhrumeen.dp@gmail.com>
2025-01-10 11:17:33 +0530

    Password reset logic

--- authentication/authentication_app/urls.py
+++ authentication/authentication_app/urls.py
@@ -1,4 +1,5 @@
 """URL configuration for authenticationApp"""
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
@@ -8,4 +9,8 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('verify-email/<uidb64>/<token>', views.verify_email, name='verify_email'),
     path('', views.home_view, name='home'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="authentication_app/password_reset_form.html"), name='password_reset'),
+    path('password_reset/done', auth_views.PasswordResetDoneView.as_view(template_name="authentication_app/password_reset_done.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="authentication_app/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="authentication_app/password_reset_complete.html"), name='password_reset_complete'),
 ]
